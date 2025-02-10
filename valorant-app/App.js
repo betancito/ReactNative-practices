314d9d1c41d12bd38906aa63f8ba3695073a4d94
betancito <jeronimobeta90@gmail.com>
2025-02-10 15:34:47 -0500

    started home view populating agents but not sure why non is displaying

--- valorant-app/App.js
+++ valorant-app/App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Image} from "react-native";
+
+import { getAgents } from "./lib/agents";
 
 export default function App() {
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    getAgents().then((agents) => {
+      setAgents(agents);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>HOLAAAA</Text>
+      {agents.map(agent => (
+        <View key={agent.uuid} style={styles.card}>
+          <Image
+            source={{ uri: agent.img}}
+            style = {{
+              width: 107,
+              height: 147,
+              boderRadius: 10,
+            }}
+          />
+          <Text>{agent.displayName}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
