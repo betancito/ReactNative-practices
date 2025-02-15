deed4576360762c4d132cb8859cb3f4fd6239e1e
betancito <jeronimobeta90@gmail.com>
2025-02-14 19:14:23 -0500

    feat: finished main view createrd components folder for componetization

--- valorant-app/App.js
+++ valorant-app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Image, Text, ScrollView} from "react-native";
+import { StyleSheet, View, Image, Text, ScrollView } from "react-native";
 
 import { getAgents } from "./lib/agents";
 
@@ -8,27 +8,22 @@ export default function App() {
   const [agents, setAgents] = useState([]);
 
   useEffect(() => {
-    getAgents().then((agents) => {
-      setAgents(agents);
-    }).
-    catch((error) => alert(error));
+    getAgents()
+      .then((agents) => {
+        setAgents(agents);
+      })
+      .catch((error) => alert(error));
   }, []);
 
   return (
     <View style={styles.container}>
       <ScrollView>
         <StatusBar style="auto" />
-        {agents.map(agent => (
+        {agents.map((agent) => (
           <View key={agent.uuid} style={styles.card}>
-            <Image
-              source={{ uri: agent.img}}
-              style = {{
-                width: 107,
-                height: 147,
-                boderRadius: 10,
-              }}
-            />
-            <Text>{agent.displayName}</Text>
+            <Image source={{ uri: agent.img }} style={styles.image} />
+            <Text style={styles.title}>{agent.displayName}</Text>
+            <Text style={styles.description}>{agent.description}</Text>
           </View>
         ))}
       </ScrollView>
@@ -42,5 +37,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  card: {
+    marginBottom: 30,
+  },
+  image: {
+    width: 107,
+    height: 147,
+    boderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
   },
 });
